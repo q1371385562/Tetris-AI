55b2b7bab07e8571796a10531b3d991c93c6ffba
q1371385562 <71457941+q1371385562@users.noreply.github.com>
2025-08-21 06:06:32 +0800

    feat: 添加简单可视化窗口

---
+++ src/visualizer.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Visualizer:
+    """简单的俄罗斯方块棋盘可视化窗口"""
+
+    def __init__(self):
+        plt.ion()
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_title("俄罗斯方块AI可视化")
+        self.im = self.ax.imshow(np.zeros((20, 10)), cmap="tab20", vmin=0, vmax=7)
+        self.ax.axis('off')
+        self.fig.show()
+
+    def update(self, field):
+        """更新当前棋盘显示"""
+        self.im.set_data(field)
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+        plt.pause(0.001)
